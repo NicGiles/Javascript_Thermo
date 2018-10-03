ca2ecfa4b6052075723bd1f10107db2adbaf228e
NicGiles <n.r.giles1@gmail.com>
2018-10-03 16:23:13 +0100

    energy usage

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -6,7 +6,7 @@ describe('Thermostat', function() {
   });
 
   it('Thermostat starts at 20', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.temperature).toEqual(DEFAULT_TEMP);
   });
 
   it('can increase temperature', function() {
@@ -22,6 +22,40 @@ describe('Thermostat', function() {
   it('cannot go below 10', function() {
     thermostat.down(17)
     expect(thermostat.temperature).toEqual(10);
+  });
+
+  it('prevents temperatures above 25 when power save is on', function() {
+    thermostat.powerSave = true
+    thermostat.up(60)
+    expect(thermostat.temperature).toEqual(MAX_TEMP_POWER_SAVE);
+  });
+
+  it('prevents temperatures above 32 when power save is off', function() {
+    thermostat.powerSave = false
+    thermostat.up(60)
+    expect(thermostat.temperature).toEqual(MAX_TEMP);
+  });
+
+  it('reverts to 20 when reset button is pressed', function() {
+    thermostat.up(4);
+    thermostat.reset();
+    expect(thermostat.temperature).toEqual(DEFAULT_TEMP);
+  });
+
+  describe('Energy Usage',function(){
+    it('Identifies when temperature is low energy usage', function(){
+      thermostat.temperature = LOW_ENERGY - 1;
+      expect(thermostat.energyUsage()).toEqual('low-usage')
+    });
+
+    it('Identifies when temperature is medium energy usage', function(){
+      expect(thermostat.energyUsage()).toEqual('medium-usage')
+    });
+
+    it('Identifies when temperature is high energy usage', function(){
+      thermostat.temperature = HIGH_ENERGY;
+      expect(thermostat.energyUsage()).toEqual('high-usage')
+    });
   });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,11 +1,19 @@
+var DEFAULT_TEMP = 20;
+var MAX_TEMP_POWER_SAVE = 25;
+var MAX_TEMP = 32;
+var LOW_ENERGY = 18;
+var HIGH_ENERGY = 25;
+
 var Thermostat = function() {
 
-  this.temperature = 20;
+  this.temperature = DEFAULT_TEMP;
+  this.powerSave = true;
 
 };
 
 Thermostat.prototype.up = function(number) {
-  this.temperature += number;
+  var maxTemp = this._getMaxTemp()
+  this.temperature += this._getIncrement(number, maxTemp)
 };
 
 Thermostat.prototype.down = function(number) {
@@ -14,4 +22,31 @@ Thermostat.prototype.down = function(number) {
   } else {
     this.temperature -= number;
   }
+};
+
+Thermostat.prototype.reset = function() {
+  this.temperature = DEFAULT_TEMP;
+};
+
+
+Thermostat.prototype._getMaxTemp = function() {
+  return this.powerSave ? MAX_TEMP_POWER_SAVE : MAX_TEMP;
+};
+
+Thermostat.prototype._getIncrement = function(number, maxTemp) {
+  if (this.temperature + number > maxTemp) {
+    return maxTemp - this.temperature;
+  } else {
+    return number;
+  }
+};
+
+Thermostat.prototype.energyUsage = function() {
+  if (this.temperature < LOW_ENERGY) {
+    return 'low-usage';
+  } else if (this.temperature < HIGH_ENERGY) {
+    return 'medium-usage';
+  } else {
+    return 'high-usage';
+  };
 };
